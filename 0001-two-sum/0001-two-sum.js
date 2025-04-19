c8023f765dfde394551e01e207620d9b42049fad
sesim <629943@naver.com>
2025-04-19 18:53:35 +0900

    Time: 0 ms (100%), Space: 55.8 MB (27.56%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
+
+    if (map.hasOwnProperty(complement)) {
+      return [ map[complement], i ];
+    }
+    map[num] = i;
+  }
+};
