80d0ca42e5aee735be54813d949351447f99e7c5
sesim <629943@naver.com>
2025-05-08 11:04:35 +0900

    Time: 61 ms (5.22%), Space: 57.2 MB (41.59%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+  const defaultInit = init;
+  let countInit = init;
+
+  function increment() {
+    return ++countInit;
+  }
+
+  function decrement() {
+    return --countInit;
+  }
+
+  function reset() {
+    countInit = defaultInit;
+    return countInit;
+  }
+
+  return {
+    increment,
+    decrement,
+    reset
+  };
+};
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
