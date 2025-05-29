3b4226f073f6c97719a363bdbd6f6eaa3cab5bac
sesim <629943@naver.com>
2025-05-29 23:45:39 +0900

    Time: 0 ms (100%), Space: 57.9 MB (42.62%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  const dummy = new ListNode(0);
+  let current = dummy;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val <= l2.val) {
+      current.next = l1;
+      l1 = l1.next;           
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;  
+  }
+
+  current.next = l1 !== null ? l1 : l2;
+
+  return dummy.next;
+};
